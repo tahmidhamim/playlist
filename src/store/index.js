c2086292e7e1f75d976c6d75bd57b0629a4c4485
Tahmid Hamim <tahmidhamim@gmail.com>
2024-04-27 14:44:10 +0600

    Init commit

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import { reset } from "./actions";
+
+const store = configureStore({
+  reducer: {
+    movies: moviesReducer,
+    songs: songsReducer,
+  },
+});
+
+export { store, reset, addMovie, removeMovie, addSong, removeSong };
